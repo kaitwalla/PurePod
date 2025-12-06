fb34878c8cefec4beeb514795aca41976a2a0ae1
Kaitlyn Concilio <git@kait.dev>
2025-12-05 20:50:53 -0800

    ugh

--- manager/frontend/src/components/EpisodeTable.tsx
+++ manager/frontend/src/components/EpisodeTable.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState, useCallback } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import {
   useReactTable,
